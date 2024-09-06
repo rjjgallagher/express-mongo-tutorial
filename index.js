5d59177ac2e1fca2f677fb3241ae42ff693384a7
rjjgallagher <rjjgallagher@gmail.com>
2024-09-05 19:54:07 -0500

    cleaned up the category selector html

--- index.js
+++ index.js
@@ -2,29 +2,33 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const Product = require('./models/product.js');
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
-    .then(() => {
-        console.log("MONGO CONNECTION OPEN!!!");
-    })
-    .catch(err => {
-        console.log("OH NO, MONGO CONNECTION ERROR!!!");
-        console.log(err);
-    });
+.then(() => {
+    console.log("MONGO CONNECTION OPEN!!!");
+})
+.catch(err => {
+    console.log("OH NO, MONGO CONNECTION ERROR!!!");
+    console.log(err);
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}));
 
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+const categories = ['fruit', 'vegetable', 'dairy'];
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
+    const products = await Product.find({});
     res.render('products/index', {products});
 });
 
 app.get('/products/new', (req, res) => {
-    res.render("products/new")
+    res.render("products/new", {categories});
 });
 
 app.post('/products', async (req, res) => {
@@ -40,6 +44,19 @@ app.get('/products/:id', async (req, res) => {
     console.log(product);
     res.render('products/details', { product });
 })
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render("products/edit", { product, categories });
+});
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+    res.redirect(`/products/${product._id}`);
+})
+
 
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000!");
